029f3a73b24c529701851b194cf24ff4ddb6c7f3
Stephen Junge <steve.junge@gmail.com>
2020-08-07 00:07:02 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_all?(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
+     block_return_values << yield(collection[i])
     i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
 my_all?([1,2,3]) {|i| i < 2}
